06253b2f25e742f087b1236d7f3a25e24b48b03a
Francisco Mancardi <francisco.mancardi@gmail.com>
2012-12-07 11:00:16 +0100

    wip #5

--- pages/bug_report_mail.php
+++ pages/bug_report_mail.php
@@ -21,6 +21,53 @@
 	require_once( plugin_config_get( 'path_erp', NULL, TRUE ) . 'core/config_api.php' );
 
 	$GLOBALS[ 't_mailboxes' ] = plugin_config_get( 'mailboxes' );
+  
+  // TESI
+  // now get config from projects table (from project_api.php)
+  $dummy_set = project_cache_all();
+ 
+  // change access key to project_id
+  $project_set = array();
+  $mail_tags = array();
+  $mail_substr = array();
+  $mail_tags_exclude = array();
+  
+  foreach($dummy_set as $idx => $projectObj)
+  {
+    	$project_set[$projectCfg['id']] = $projectCfg;
+		$key2use = 'mail_tag';
+    	if(isset($projectObj[$key2use]) && !is_null($projectObj[$key2use]) && ($t_mail_tag = trim($projectObj[$key2use])) != '')
+    	{
+    		$mail_tags[$t_mail_tag] = array('id' => $projectObj['id'], 'name' => $projectObj['name'] );  
+    	}
+    	
+    	// TESI
+    	$key2use = 'mail_substr';
+    	if(isset($projectObj[$key2use]) && !is_null($projectObj[$key2use]) && (trim($projectObj[$key2use])) != '')
+    	{
+    		$yumyum = explode(':',$projectObj[$key2use]);
+    		foreach($yumyum as $composite)
+    		{
+    			$xd = explode('=',$composite);
+    			$mail_substr[$projectObj['id']][] = array('project_name' => $projectObj['name'], 
+    													  'needle' => $xd[0], 'len' => $xd[1]);  
+    		}	
+    	}
+
+		// TESI
+		$key2use = 'mail_tag_exclude';
+    	if(isset($projectObj[$key2use]) && !is_null($projectObj[$key2use]) && trim($projectObj[$key2use]) != '')
+    	{
+    		$yumyum = explode(',',$projectObj[$key2use]);
+    		foreach($yumyum as $ordos)
+    		{
+    			$mail_tags_exclude[$projectObj['id']][] = array('project_name' => $projectObj['name'], 
+    															'needle' => $ordos);  
+    		}	
+    	}
+  }
+  unset($dummy);
+  
 
 	$t_mail_mantisbt_url_fix = plugin_config_get( 'mail_mantisbt_url_fix', '' );
 	if ( php_sapi_name() === 'cli' && !is_blank( $t_mail_mantisbt_url_fix ) )
@@ -31,12 +78,19 @@
 	$t_mailbox_api_index = ERP_get_mailbox_api_name();
 
 	$GLOBALS[ $t_mailbox_api_index ] = new ERP_mailbox_api;
+  
+  // TESI
+  $GLOBALS[ $t_mailbox_api_index ]->set_mail_tags($mail_tags);
+  $GLOBALS[ $t_mailbox_api_index ]->set_mail_substr($mail_substr);
+  $GLOBALS[ $t_mailbox_api_index ]->set_mail_tags_exclude($mail_tags_exclude);  // 20120911
+
 
 	ini_set( 'memory_limit', -1 );
 	set_time_limit( 0 );
 
 	foreach ( $GLOBALS[ 't_mailboxes' ] as $t_mailbox )
 	{
+		echo "\n (tesi)Mail Box Description:" . $t_mailbox['description'] . "\n";
 		$GLOBALS[ $t_mailbox_api_index ]->process_mailbox( $t_mailbox );
 	}
 
